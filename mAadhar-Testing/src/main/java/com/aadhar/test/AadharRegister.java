ff01c369b2160d57b86757d5cc4491960ce08434
Piyush Garg <88052849+piyushgarg19@users.noreply.github.com>
2023-09-14 13:08:55 +0530

    Added m-aadharapplication-testing using TestNgClass

---
+++ mAadhar-Testing/src/main/java/com/aadhar/test/AadharRegister.java
@@ -0,0 +1,44 @@
+package com.aadhar.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AadharRegister {
+	static WebDriver webdriver = null;
+
+	@Test
+	public void ChromeBrowser() {
+		webdriver = new ChromeDriver();
+		webdriver.manage().window().maximize();
+		System.out.println("Chrome Browser is Opened!!");
+
+	}
+
+	@Test
+	public void TestHome() {
+		webdriver.get("http://localhost:4200/login");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		webdriver.findElement(By.xpath("//div[@class='login-container']")).click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		webdriver.get("http://localhost:4200/user-register");
+
+		webdriver.findElement(By.id("name")).sendKeys("Ayush Garg");
+		webdriver.findElement(By.id("dob")).sendKeys("16/01/1996");
+		webdriver.findElement(By.id("emailid")).sendKeys("gargayush1696@gmail.com");
+		webdriver.findElement(By.id("address")).sendKeys("Janakpuri, Deoband");
+		webdriver.findElement(By.id("mobileno")).sendKeys("9878765432");
+		webdriver.findElement(By.id("gender")).sendKeys("Male");
+	}
+
+}
